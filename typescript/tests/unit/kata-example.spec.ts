d11e17f7940ee511e1a04df39c4ca2d1c8b646e6
Chemaclass <chemaclass@outlook.es>
2023-03-17 19:10:50 +0100

    Red: should return one when push one time

--- typescript/tests/unit/kata-example.spec.ts
+++ typescript/tests/unit/kata-example.spec.ts
@@ -1,4 +1,4 @@
-import { Stack } from '../../src/Stack'
+import {Stack} from '../../src/Stack'
 
 describe('Stack', () => {
     it('should be empty when initialized', () => {
@@ -6,9 +6,15 @@ describe('Stack', () => {
 
         expect(stack.isEmpty()).toBe(true)
     });
-    it('should return zero when initialized',()=>{
+    it('should return zero when initialized', () => {
         const stack = new Stack()
 
         expect(stack.size()).toBe(0)
+    });
+
+    it('should return one when push one time', () => {
+        const stack = new Stack()
+        stack.push('foo')
+        expect(stack.size()).toBe(1)
     });
 });
