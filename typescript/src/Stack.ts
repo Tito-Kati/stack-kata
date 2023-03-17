12580fb71ac1acde3fb3407296bb8a64d32b1376
Chemaclass <chemaclass@outlook.es>
2023-03-17 19:14:57 +0100

    Green: should return one when push one time

--- typescript/src/Stack.ts
+++ typescript/src/Stack.ts
@@ -1,8 +1,15 @@
 export class Stack {
+    private _size : number = 0
+
     isEmpty (): boolean {
         return true
     }
+
     size (): number{
-        return 0
+        return this._size
+    }
+
+    push (foo: any) {
+        this._size = 1
     }
 }
