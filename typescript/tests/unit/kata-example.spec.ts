126dc163171be913d3f3f8d238363502416d4513
katarn <katarn@katarn.es>
2023-03-21 18:30:31 +0100

    Refactor: Group tests by methods

--- typescript/tests/unit/kata-example.spec.ts
+++ typescript/tests/unit/kata-example.spec.ts
@@ -5,32 +5,34 @@ describe('Stack', () => {
 
     beforeEach(() => {
         stack = new Stack()
-    });
-
-    it('should be empty when initialized', () => {
-        expect(stack.isEmpty()).toBe(true)
-    });
-
-    it('should not be empty when pushed on time', () => {
-        stack.push('foo')
-        expect(stack.isEmpty()).toBe(false)
     })
 
-    it('should return zero when initialized', () => {
-        expect(stack.size()).toBe(0)
-    });
+    describe('isEmpty', () => {
+        it('should be empty when initialized', () => {
+            expect(stack.isEmpty()).toBe(true)
+        })
+    
+        it('should not be empty when pushed on time', () => {
+            stack.push('foo')
+            expect(stack.isEmpty()).toBe(false)
+        })
+    })
 
-    it('should return one when pushed one time', () => {
-        stack.push('foo')
-        expect(stack.size()).toBe(1)
-    });
-
-    it('should return three when pushed three times', () => {
-        stack.push('foo')
-        stack.push('faa')
-        stack.push('fuu')
-        expect(stack.size()).toBe(3)
-    });
-
-
-});
+    describe('size', () => {
+        it('should return zero when initialized', () => {
+            expect(stack.size()).toBe(0)
+        })
+    
+        it('should return one when pushed one time', () => {
+            stack.push('foo')
+            expect(stack.size()).toBe(1)
+        })
+    
+        it('should return three when pushed three times', () => {
+            stack.push('foo')
+            stack.push('faa')
+            stack.push('fuu')
+            expect(stack.size()).toBe(3)
+        })
+    })
+})
